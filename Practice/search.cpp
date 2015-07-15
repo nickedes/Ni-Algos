d8ab0a8af8b5ea7abbd1bcb00f125d2800b8f6b7
nikhil <nkmittal4994@gmail.com>
2015-07-15 23:41:30 +0530

    Implemented search,ising binary search :tada:

--- Practice/search.cpp
+++ Practice/search.cpp
@@ -2,14 +2,27 @@
 #include <iostream>
 using namespace std;
 
+int binarySearch(int arr[], int low, int high, int num)
+{
+   if (high < low)
+       return -1;
+   int mid = (low + high)/2;
+   if (num == arr[mid])
+     return mid;
+   if (num > arr[mid])
+     return binarySearch(arr, (mid + 1), high, num);
+   else
+     return binarySearch(arr, low, (mid -1), num);
+}
+
 int main()
 {
-	int n,arr[100],i,val,pivot;
+	int n, arr[100], i, val, pivot, flag = 0;
 	cout << "Enter N:";
 	cin >> n;
 
-	cout << "Enter "<<n-1<<" no.s:";
-	for(i = 0; i < n-1; i++)
+	cout << "Enter "<<n<<" no.s:";
+	for(i = 0; i < n; i++)
 	{
 		cin >> arr[i];
 	}
@@ -17,13 +30,31 @@ int main()
 	cin >> val;
 
 	// First finding the Pivot
-	for(i = 0; i < n-2; i++)
+	for(i = 0; i < n-1; i++)
 	{
 		if (arr[i] > arr[i+1])
 		{
 			pivot = i;
+			flag = 1;
 			break;
 		}
 	}
-	cout << "Pivot:" << arr[i];
+	if(!flag)
+		pivot = -1;
+
+	// If we didn't find a pivot, then array is not rotated at all
+	if (pivot == -1)
+		return binarySearch(arr, 0, n-1, val);
+	
+	cout << "Index of the element: "; 
+	// If we found a pivot, then first compare with pivot and then
+    // search in two subarrays around pivot
+	if (arr[pivot] == val)
+		cout << pivot;
+	if (arr[0] <= val)
+		cout << binarySearch(arr, 0, pivot-1, val);
+	else
+		cout << binarySearch(arr, pivot+1, n-1, val);
+
+	return 0;
 }
