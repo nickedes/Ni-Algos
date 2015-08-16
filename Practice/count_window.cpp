fa6d5d1d7383038a1e5de466a8e81ec68d7c82a4
nikhil <nkmittal4994@gmail.com>
2015-08-16 19:09:14 +0530

    solved using hashmap, in linear time complexity.

--- Practice/count_window.cpp
+++ Practice/count_window.cpp
@@ -7,7 +7,7 @@ numbers in all windows of size k.
 #include <bits/stdc++.h>
 using namespace std;
 
-map<int, int> m;
+map<int, int> hashmap;
 int main()
 {
 	int array[100]={0}, num, window_size, i;
@@ -23,9 +23,25 @@ int main()
 
 	for(i = 0; i < window_size; i++)
 	{
-		m[array[i]]++;
+		hashmap[array[i]]++;
 	}
 	// print count of distinct no.s in first window.
-	cout << m.size() << endl;
+	cout << hashmap.size() << endl;
+
+	map<int,int> :: iterator iter;
+	for(i = window_size; i < num; i++)
+	{
+		// Update hashmap for the element removed from window.
+		// If single occurence then removed, else decremented it's count.
+		iter = hashmap.find(array[i-window_size]);
+		if(iter -> second == 1)
+			hashmap.erase(iter);
+		else
+			hashmap[array[i-window_size]] = hashmap[array[i-window_size]]-1;
+		// update hash map for the new element in the window.
+		hashmap[array[i]]++;
+		// count of distinct no.s in the present window.
+		cout << hashmap.size() << endl;
+	}
 	return 0;
 }
