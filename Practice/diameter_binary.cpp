b9bca6b8ec90191c4222ade97677ef0cc3ef8eab
nikhil <nkmittal4994@gmail.com>
2015-09-11 02:32:46 +0530

    Calculation of diameter of tree,and using height also

--- Practice/diameter_binary.cpp
+++ Practice/diameter_binary.cpp
@@ -19,6 +19,30 @@ node* getNode(int value)
 	return temp;
 }
 
+int height(struct node *temp)
+{
+	if(temp == NULL)
+		return 0;
+	else
+		return 1 + max(height(temp->left),height(temp->right));
+}
+
+int diameter(struct node* temp)
+{
+	if(temp == NULL)
+		return 0;
+
+	/* get the diameter of left and right sub-trees */
+	int left_dia = diameter(temp->left);
+	int rig_dia = diameter(temp->right);
+
+	/* get the height of left and right sub-trees */
+	int left_ht = height(temp->left);
+	int right_ht = height(temp->right);
+
+	return max(left_ht+right_ht+1,max(left_dia,rig_dia));
+}
+
 int main()
 {
 	struct node *root = getNode(1);
@@ -27,4 +51,5 @@ int main()
 	root->left->left  = getNode(4);
 	root->left->right = getNode(5);
 
+	cout << "Diameter : " << diameter(root);	
 }
