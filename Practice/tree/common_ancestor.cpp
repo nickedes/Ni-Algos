d32d18ab651e23ab9927713d88d0c32c805f477d
nikhil <nkmittal4994@gmail.com>
2015-09-29 09:15:41 +0530

    initialising : findIng LCA

--- Practice/tree/common_ancestor.cpp
+++ Practice/tree/common_ancestor.cpp
@@ -6,6 +6,28 @@ NOTE: This is not necessarily a binary search tree.
 #include <iostream>
 using namespace std;
 
+class node
+{
+public:
+	int data;
+	node *left;
+	node *right;
+};
+
+node* newnode(int key)
+{
+    node *temp = new node;
+    temp->data = key;
+    temp->left = temp->right = NULL;
+    return temp;
+}
+
+node *findLCA(node *root, int n1, int n2)
+{
+	// finding LCA
+	// Assuming n1 and n2 exist in tree.
+}
+
 int main()
 {
 	return 0;
