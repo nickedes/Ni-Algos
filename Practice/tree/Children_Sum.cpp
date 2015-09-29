0ee5d1b866c2dc3bd275890cab82419817216cd6
nikhil <nkmittal4994@gmail.com>
2015-09-30 02:02:55 +0530

    chidlren sum problem : class creation and tree

---
+++ Practice/tree/Children_Sum.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+class node
+{
+public:
+	int data;
+	node *left;
+	node *right;
+};
+
+node* newNode(int data)
+{
+	node *ptr;
+	ptr->data = data;
+	ptr->left = NULL;
+	ptr->right = NULL;
+	return(ptr);
+}
+
+int main()
+{
+	node *root = newNode(50);
+	root->left        = newNode(7);
+	root->right       = newNode(2);
+	root->left->left  = newNode(3);
+	root->left->right = newNode(5);
+	root->right->left  = newNode(1);
+	root->right->right = newNode(30);
+}
