c6d8cabe7760ae1ce8dc9d14156cfaa736efc1d0
nikhil <nkmittal4994@gmail.com>
2015-10-04 15:01:26 +0530

    Heap - insertion (complete binary tree)

--- Practice/tree/kth_largest.cpp
+++ Practice/tree/kth_largest.cpp
@@ -1,5 +1,5 @@
 /*
-k largest(or smallest) elements in an array.
+kth largest(or smallest) elements in an array.
  */
 #include <iostream>
 using namespace std;
@@ -12,14 +12,70 @@ public:
 	node *right;
 };
 
+node* newNode(int key)
+{
+	node *ptr = new node;
+	ptr->data = key;
+	ptr->left = ptr->right = NULL;
+	return ptr;
+}
+
 // add to heap tree
-void insert(node *root);
+void insert(node *root, int key)
+{
+	node *temp = newNode(key);
+	if(root == NULL)
+		root = temp;
+	else
+	{
+		if(root->left == NULL)
+		{
+			root->left = temp;
+		}
+		else if(root->right == NULL)
+		{
+			root->right = temp;
+		}
+		else
+		{
+			insert(root->left, key);
+			insert(root->right, key);
+		}
+	}
+}
 
 // heapify - reach appropriate position in tree.
-void heapify(node *root);
+// void heapify(node *root);
+
+void inorder(node *root)
+{
+	if(root == NULL)
+		return;
+	inorder(root->left);
+	cout << root->data;
+	inorder(root->right);
+}
 
 int main()
 {
 	node *root = NULL;
+	int ch;
+	while(1)
+	{
+		cout << "1. Insert" << "2. Display" << "3. exit";
+		cin >> ch;
+		switch(ch)
+		{
+			case 1: //insert
+			break;
+			case 2: //display
+			break;
+			case 3://exit
+			return 0;
+			break;
+			default:
+			break;
+		}
+	}
 	return 0;
 }
