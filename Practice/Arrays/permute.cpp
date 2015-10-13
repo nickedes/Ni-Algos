40c044d8299ffb40c3be8c42f061e517281ba8d5
nikhil <nkmittal4994@gmail.com>
2015-10-13 10:56:50 +0530

    all Permutations of a string

--- Practice/Arrays/permute.cpp
+++ Practice/Arrays/permute.cpp
@@ -2,19 +2,33 @@
 #include <string.h>
 using namespace std;
 
+void swap(char array[], int x, int y)
+{
+	char temp;
+	temp = array[x];
+	array[x] = array[y];
+	array[y] = temp;
+}
+
 void all_perms(char array[], int low, int high)
 {
 	if(low == high)
-		cout << array[low];
+		cout << array << endl;
 	else
 	{
-		// all permutation go here harry! 
+		// all permutation go here harry!
+		for(int i = low; i <= high; i++)
+		{
+			swap(array, low, i);
+			all_perms(array, low+1, high);
+			swap(array, low, i);
+		}
 	}
 }
 
 int main()
 {
-	char array[] = "ABCDE";
+	char array[] = "ABC";
 
 	all_perms(array, 0, strlen(array)-1);
 
