17245ca65fef4832489a8da4328b9f0fece2200d
nikhil <nkmittal4994@gmail.com>
2015-10-30 06:13:21 +0530

    chap1 Problem 2 - Method added...

---
+++ Practice/Ctci/Ch 1/1.2 Check Permutation.cpp
@@ -0,0 +1,24 @@
+/*
+
+ */
+
+#include <iostream>
+using namespace std;
+
+bool check_permutation(char first[], char second[])
+{
+	if(//string len same)
+	{
+		// create hashmap of one string and keep count of chars
+		// check the hashmap with the second string
+	}
+	return 1;
+}
+
+int main()
+{
+	// Take 2 strings
+	
+	// call check permutation
+	return 0;
+}
