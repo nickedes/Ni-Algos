2adcbf963520c396bb0da13d7600c4d9df0c7de5
nikhil <nkmittal4994@gmail.com>
2015-10-30 20:17:49 +0530

    Main method for checking permutation implemented using a hashmap

--- Practice/Ctci/Ch 1/1.2 Check Permutation.cpp
+++ Practice/Ctci/Ch 1/1.2 Check Permutation.cpp
@@ -3,14 +3,35 @@
  */
 
 #include <iostream>
+#include <string.h>
+#include <bits/stdc++.h>
 using namespace std;
+
+#define SIZE 10
 
 bool check_permutation(char first[], char second[])
 {
-	if(//string len same)
+	if(strlen(first) == strlen(second))
 	{
 		// create hashmap of one string and keep count of chars
 		// check the hashmap with the second string
+		map<char, int> hashmap;
+		for(int i=0; first[i] != '\0'; i++)
+		{
+			hashmap[first[i]]++;
+		}
+		map<char, int> :: iterator iter;
+		for(int i = 0; second[i] != '\0'; i++)
+		{
+			iter = hashmap.find(second[i]);
+			if(iter->second == 1)
+				hashmap.erase(iter);
+			else if(iter->second == 0)
+				return 0;
+			else
+				hashmap[second[i]]--;
+		}
+		return 1;
 	}
 	return 1;
 }
