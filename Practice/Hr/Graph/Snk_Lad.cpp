014e0ac4df1227d3c102c25ec2cc22ed702a1c53
nikhil <nkmittal4994@gmail.com>
2015-11-10 19:43:11 +0530

    Partial solution of snk ladder

--- Practice/Hr/Graph/Snk_Lad.cpp
+++ Practice/Hr/Graph/Snk_Lad.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
 
 int main()
@@ -19,7 +20,47 @@ int main()
 		for (int j = 0; j < m; ++j)
 			cin >> snake[j][0] >> snake[j][1];
 		int moves = 0;
-		//solve
+		// find the ladder with max jump
+		int max = ladder[0][1] - ladder[0][0], index = 0;
+		for(int j = 1; j < n; j++)
+		{
+			if(max < ladder[j][1] - ladder[j][0])
+			{
+				index = j;
+				max = ladder[j][1] - ladder[j][0];
+			}
+				
+		}
+
+		int start = 1, end = 100;
+		moves += (ladder[index][0]>6)?ceil(ladder[index][0]/6.0):1;
+		start =  ladder[index][1];
+
+		// find max snake
+		max = snake[0][0];
+		int max2 = 0;
+		for(int j = 1; j < m; j++)
+		{
+			if(max > snake[j][0])
+			{
+				max = snake[j][0];
+				index = j;
+			}
+		}
+
+		for(int j = 0; j < m; j++)
+		{
+			if(start < snake[j][0] && snake[j][0] < max)
+			{
+				max2 = snake[j][0];
+				index = j;
+			}
+		}
+		if(start < max)
+		{
+			// TODO: snake concept
+		}
+		moves += (100 - start) > 6 ? ceil((100 - start)/6.0) : 1; 
 		cout << moves<< endl;
 	}
 
