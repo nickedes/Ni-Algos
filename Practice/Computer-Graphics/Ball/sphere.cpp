0fe72938087cf1d845de40b98888ef797a92e827
nikhil <nkmittal4994@gmail.com>
2015-12-17 22:08:30 +0530

    Sphere translating along y-axis

--- Practice/Computer-Graphics/Ball/sphere.cpp
+++ Practice/Computer-Graphics/Ball/sphere.cpp
@@ -1,3 +1,4 @@
+// sphere with moving up
 #include "GL/freeglut.h"
 #include "GL/gl.h"
 #include <math.h>
@@ -6,6 +7,7 @@ using namespace std;
 
 GLfloat xRotated, yRotated, zRotated;
 GLdouble radius=1;
+GLfloat y = 0.0;
 void init(void)
 {
 	glClearColor(0.0, 0.0, 0.0, 0.0);
@@ -16,18 +18,18 @@ void DrawSphere()
 {
 	// traslate the draw by z = -4.0
     // Note this when you decrease z like -8.0 the drawing will looks far , or smaller.
-    glTranslatef(0.0,0.0,-10.0);
+    glTranslatef(0.0,y,-10.0);
     // Red color used to draw.
-    glColor3f(1.0, 1.0, 0.0); 
+    glColor3f(1.0, 0.0, 1.0); 
     // changing in transformation matrix.
     // rotation about X axis
-    glRotatef(xRotated,1.0,0.0,0.0);
+    // glRotatef(xRotated,1.0,0.0,0.0);
     // rotation about Y axis
-    glRotatef(yRotated,0.0,1.0,0.0);
+    // glRotatef(yRotated,0.0,1.0,0.0);
     // rotation about Z axis
-    glRotatef(zRotated,0.0,0.0,1.0);
+    // glRotatef(zRotated,0.0,0.0,1.0);
     // scaling transfomation 
-    glScalef(1.0,1.0,1.0);
+    // glScalef(1.0,1.0,1.0);
     // built-in (glut library) function , draw you a sphere.
     glutSolidSphere(radius,20,20);
 }
@@ -52,10 +54,19 @@ void reshape(int w, int h)
  //    glViewport(0,0,w,h);  //Use the whole window for rendering
 }
 
+void show()
+{
+	y += 0.1;
+	if( y > 5.0)
+		y = -5.0;
+	cout << y << endl;
+	glutPostRedisplay();
+}
+
 void mouse(int button, int state, int x, int y)
 {
 	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
-		glutIdleFunc(NULL);
+		glutIdleFunc(show);
 	else if (button == GLUT_MIDDLE_BUTTON && state == GLUT_DOWN)
 		glutIdleFunc(NULL);
 }
