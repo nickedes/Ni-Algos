81027dbd28bc2bda60b459d92e3789d3dca39102
nikhil <nkmittal4994@gmail.com>
2016-02-07 18:23:09 +0530

    for insertion sort..

---
+++ Practice/Hr/Sorting/running_time.cpp
@@ -0,0 +1,19 @@
+/*
+Problem : https://www.hackerrank.com/challenges/insertionsort1
+ */
+
+#include <iostream>
+using namespace std;
+
+#define SIZE 1001
+
+int main()
+{
+	int n, arr[SIZE], j, key, shift = 0;
+
+	cin >> n;
+	for (int i = 0; i < n; ++i)
+		cin >> arr[i];
+	
+	return 0;
+}
