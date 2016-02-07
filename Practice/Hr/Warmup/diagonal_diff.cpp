67af8fe31aeecbfb757f8e7f0c0baf3735d4a235
nikhil <nkmittal4994@gmail.com>
2016-02-07 13:13:11 +0530

    calculating sum of primary diagonal

--- Practice/Hr/Warmup/diagonal_diff.cpp
+++ Practice/Hr/Warmup/diagonal_diff.cpp
@@ -7,5 +7,20 @@ using namespace std;
 
 int main()
 {
+	int n, j = 0, sum_prim, sum_sec;
+	cin >> n;
+	sum_prim = 0; // sum of primary diagonal
+	sum_sec = 0; // sum of secondary diagonal
+	for (int i=0; i < n*n; i++)
+	{
+		int x;
+		cin >> x;
+		if (i == 0 || i % j == 0)
+		{
+			sum_prim += x;
+			j += n+1;
+		}
+	}
+	cout << sum_prim;
 	return 0;
 }
