3df4f8ce1901364b942b49474b70f8a00b5d99d5
nikhil <nkmittal4994@gmail.com>
2016-02-07 12:48:59 +0530

    Solved sum of big no.s

--- Practice/Hr/Warmup/big_sum.cpp
+++ Practice/Hr/Warmup/big_sum.cpp
@@ -7,5 +7,14 @@ using namespace std;
 
 int main()
 {
+	long long int n, sum = 0;
+	cin >> n;
+	for (long long int i = 0; i < n; ++i)
+	{
+		long long int x ;
+		cin >> x;
+		sum += x;
+	}
+	cout << sum;
 	return 0;
 }
