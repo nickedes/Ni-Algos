6e8c1e4532cdf407d55f1b5200017c471484aa5c
nikhil <nkmittal4994@gmail.com>
2016-02-07 18:13:09 +0530

    Insertion sort for sorting unsorted ele

--- Practice/Hr/Sorting/insertion_sort1.cpp
+++ Practice/Hr/Sorting/insertion_sort1.cpp
@@ -13,13 +13,16 @@ void printarr(int arr[SIZE], int n)
 	{
 		cout << arr[i] << " ";
 	}
+	cout << endl;
 }
 int main()
 {
-	int n, arr[SIZE], e;
+	int n, arr[SIZE], e, flag = 0;
 	// e is the unsorted element
 	cin >> n;
-	e = arr[n-1]
+	for (int i = 0; i < n; ++i)
+		cin >> arr[i];
+	e = arr[n-1];
 	for (int i = n-2; i >= 0; --i)
 	{
 		if(arr[i] > e)
@@ -27,10 +30,13 @@ int main()
 		else
 		{
 			arr[i+1] = e;
+			flag = 1;
 			break;
 		}
 		printarr(arr, n);
 	}
-	printarr(arr, n)
+	if(flag == 0)
+		arr[0] = e;
+	printarr(arr, n);
 	return 0;
 }
