ef877d2ed4f6b2815c6ade99a89bc459e4467a7f
nikhil <nkmittal4994@gmail.com>
2016-02-07 18:23:53 +0530

    running time for insertion sort

--- Practice/Hr/Sorting/running_time.cpp
+++ Practice/Hr/Sorting/running_time.cpp
@@ -14,6 +14,22 @@ int main()
 	cin >> n;
 	for (int i = 0; i < n; ++i)
 		cin >> arr[i];
-	
+	for (i = 1; i < n; i++)
+    {
+       key = arr[i];
+       j = i-1;
+
+       /* Move elements of arr[0..i-1], that are
+          greater than key, to one position ahead
+          of their current position */
+       while (j >= 0 && arr[j] > key)
+       {
+           arr[j+1] = arr[j];
+           j = j-1;
+           shift++;
+       }
+       arr[j+1] = key;
+    }
+    cout << shift;
 	return 0;
 }
