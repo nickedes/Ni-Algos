33d0cd755cc80ff6352ddb3e6134b88b1ad97135
nikhil <nkmittal4994@gmail.com>
2016-02-07 13:47:57 +0530

    difference of the sum of primary nd seconday diagonals

--- Practice/Hr/Warmup/diagonal_diff.cpp
+++ Practice/Hr/Warmup/diagonal_diff.cpp
@@ -3,14 +3,16 @@ Problem : https://www.hackerrank.com/challenges/diagonal-difference
  */
 
 #include <iostream>
+#include <stdlib.h>
 using namespace std;
 
 int main()
 {
-	int n, j = 0, sum_prim, sum_sec;
+	int n, j = 0, k, sum_prim, sum_sec;
 	cin >> n;
 	sum_prim = 0; // sum of primary diagonal
 	sum_sec = 0; // sum of secondary diagonal
+	k = n-1;
 	for (int i=0; i < n*n; i++)
 	{
 		int x;
@@ -20,7 +22,12 @@ int main()
 			sum_prim += x;
 			j += n+1;
 		}
+		if(i!=0 && i % k == 0 && i!= n*n-1)
+		{
+			sum_sec += x;
+			k += n-1;
+		}
 	}
-	cout << sum_prim;
+	cout << abs(sum_prim-sum_sec);
 	return 0;
 }
