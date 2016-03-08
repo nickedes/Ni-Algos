865ddab6e7e166431c7156a6614ea6c19e8ffbf4
nikhil <nkmittal4994@gmail.com>
2016-03-09 02:03:56 +0530

    a simpler method for finding the index

--- Practice/Hr/Strings/Palindrome_Index.cpp
+++ Practice/Hr/Strings/Palindrome_Index.cpp
@@ -6,6 +6,14 @@ Problem : https://www.hackerrank.com/challenges/palindrome-index
 #include <string>
 using namespace std;
 
+int palindrome_check(string s, int start, int end)
+{
+	for (int i = start, j = end; i < j; ++i, --j)
+		if(s[i] != s[j])
+			return 0; // not pal
+	return 1; // pal
+}
+
 int main()
 {
 	int test;
@@ -15,76 +23,24 @@ int main()
 		string s;
 		cin >> s;
 
-		int index1 = -1;
+		int index = -1;
 		for (int i = 0, j = s.length() - 1; i < j; ++i, --j)
 		{
 			if (s[i] != s[j])
 			{
-				if(s[i+1] == s[j])
+				if(palindrome_check(s, i, j-1))
 				{
-					index1 = i;
-					j++;
-				}
-				else if(s[i] == s[j-1])
-				{
-					index1 = j;
+					index = j;
 					i--;
 				}
-			}
-		}
-		int index2 = -1;
-		for (int i = 0, j = s.length() - 1; i < j; ++i, --j)
-		{
-			if (s[i] != s[j])
-			{
-				if(s[i] == s[j-1])
+				else
 				{
-					index2 = j;
-					i--;
-				}
-				else if(s[i+1] == s[j])
-				{
-					index2 = i;
+					index = i;
 					j++;
 				}
 			}
 		}
-		if (index1 == index2)
-			cout << index1 << endl;
-		else
-		{
-			int index = -1;
-			// checking wid index1
-			for (int i = 0, j = s.length() - 1; i < j; ++i, --j)
-			{
-				if(i == index1)
-					j++;
-				else if(j == index1)
-					i--;
-				if(s[i]!=s[j])
-				{
-					index = 1; // wrong index1
-					break;
-				}
-			}
-			// check with index2
-			for (int i = 0, j = s.length() - 1; i < j; ++i, --j)
-			{
-				if(i == index2)
-					j++;
-				else if(j == index2)
-					i--;
-				if(s[i]!=s[j])
-				{
-					index = 2; // wrong index2
-					break;
-				}
-			}
-			if(index == 1)
-				cout << index2 << endl;
-			else if(index == 2)
-				cout << index1 << endl;
-		}
+		cout << index << endl;
 	}
 	return 0;
 }
