618bd2179e7a2873cd20795b40348cbffd0b4655
nikhil <nkmittal4994@gmail.com>
2016-03-08 22:31:32 +0530

    Solved anagrams

--- Practice/Hr/Strings/Sherlock_and_Anagrams.cpp
+++ Practice/Hr/Strings/Sherlock_and_Anagrams.cpp
@@ -1,21 +1,22 @@
 /*
-Problem : https://www.hackerrank.com/challenges/two-strings
+Problem : https://www.hackerrank.com/challenges/sherlock-and-anagrams
 */
 
 #include <iostream>
+#include <string>
 using namespace std;
 
-int check_anagram(char a[], char b[])
+int check_anagram(string s, int i, int j, int len)
 {
-   int first[26] = {0}, second[26] = {0}, c = 0;
+   int first[26] = {0}, second[26] = {0}, c = i;
 
-   while (a[c] != '\0') {
-      first[a[c]-'a']++;
+   while (c < i + len) {
+      first[s[c]-'a']++;
       c++;
    }
-   c = 0;
-   while (b[c] != '\0') {
-      second[b[c]-'a']++;
+   c = j;
+   while (c < j + len) {
+      second[s[c]-'a']++;
       c++;
    }
 
@@ -27,11 +28,30 @@ int check_anagram(char a[], char b[])
    return 1;
 }
 
-
 int main()
 {
 	int test;
 	cin >> test;
+
+	while (test--)
+	{
+		string s;
+		cin >> s;
+		int count = 0;
+
+		for (int len = 1; len < s.length(); ++len)
+		{
+			for (int i = 0; i < s.length() - len; i++)
+			{
+				for (int j = i+1; j < s.length() - len + 1; ++j)
+				{
+					if (check_anagram(s, i, j, len) == 1)
+						count++;
+				}
+			}
+		}
+		cout << count << endl;
+	}
 
 	return 0;
 }
