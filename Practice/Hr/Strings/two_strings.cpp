92822b87012a63f8fbc9101f2cf063dfaf07d9c8
nikhil <nkmittal4994@gmail.com>
2016-03-08 20:51:09 +0530

    solved 2 strings problem

--- Practice/Hr/Strings/two_strings.cpp
+++ Practice/Hr/Strings/two_strings.cpp
@@ -7,5 +7,29 @@ using namespace std;
 
 int main()
 {
+	int test;
+	cin >> test;
+	for (int i = 0; i < test; ++i)
+	{
+		string a, b;
+		int check = 0;
+		cin >> a;
+		cin >> b;
+		char map[26] = {0};
+		for (int j = 0; a[j] != '\0'; ++j)
+			map[a[j]-97] = 1;
+		for (int j = 0; b[j] != '\0'; ++j)
+		{
+			if(map[b[j]-97] == 1)
+			{
+				check = 1;
+				cout << "YES";
+				break;
+			}
+		}
+		if(check == 0)
+			cout << "NO";
+		cout << endl;
+	}
 	return 0;
 }
