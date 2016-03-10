42846c71d0f464505a173f154b0b6978a149d031
nikhil <nkmittal4994@gmail.com>
2016-03-10 22:26:16 +0530

    solved reverse shuffle merge

--- Practice/Hr/Strings/Reverse_Shuffle_Merge.cpp
+++ Practice/Hr/Strings/Reverse_Shuffle_Merge.cpp
@@ -5,48 +5,42 @@ Problem : https://www.hackerrank.com/challenges/reverse-shuffle-merge
 #include <iostream>
 using namespace std;
 
-int get_min_char(int freq[])
-{
-	for (int i = 0; i < 26; ++i)
-		if(freq[i] > 0)
-			return i;
-	return 0;
-}
-
 int main()
 {
 	string s;
 	cin >> s;
-	int freq[26] = {0}, A_freq[26] = {0}, min;
+	int freq[26] = {0}, added[26]={0}, seen[26]={0};
 	char A[5000];
 	for (int i = 0; i < s.length(); ++i)
 		freq[s[i]-'a']++;
 	for (int i = 0; i < 26; ++i)
-	{	
-		A_freq[i] = freq[i]/2;
-	}
-	int j = 0, track = 26;
-	min = get_min_char(A_freq);
-	for (int i = s.length()-1; i >= 0; --i)
+		freq[i] = freq[i]/2;
+
+	int j = 0, check = s.length()-1; // j to track last minimum added
+	for (int k = 0; k < s.length()/2; ++k)
 	{
-		char c = s[i];
-		freq[c-'a']--;
-		if(min == c-'a')
+		A[k] = (char)('z'+1);
+		for (int i = check; i >= 0; --i)
 		{
-			A[j++] = c;
-			A_freq[c-'a']--;
-		}
-		else
-		{		
-			if(freq[c-'a'] >= A_freq[c-'a'])
-				continue;
-			else
+			int c = s[i]-'a';
+			if(s[i] < A[k] && (freq[c]-added[c])>0)
 			{
-				A[j++] = c;
-				A_freq[c-'a']--;
+				A[k]=s[i];
+				j = i;
+			}
+			seen[c]++;
+			if((seen[c]-added[c]) > freq[c])
+			{
+				while(i<j)
+				{
+					seen[s[i]-'a']--;
+					i++;
+				}
+				check = j-1;
+				break;
 			}
 		}
-		min = get_min_char(A_freq);
+		added[A[k]-'a']++;
 	}
 	for (int i = 0; i < s.length()/2; ++i)
 		cout << A[i];
