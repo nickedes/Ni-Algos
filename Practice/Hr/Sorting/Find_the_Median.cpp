242b8f1f50458655050767639cf51f6c57217838
nikhil <nkmittal4994@gmail.com>
2016-03-12 19:28:58 +0530

    Finding the median in 0(N)

--- Practice/Hr/Sorting/Find_the_Median.cpp
+++ Practice/Hr/Sorting/Find_the_Median.cpp
@@ -1,12 +1,29 @@
 /*
-Problem : https://www.hackerrank.com/challenges/closest-numbers
+Problem : https://www.hackerrank.com/challenges/find-median
  */
 
 #include <iostream>
-#include <stdlib.h>
 using namespace std;
 
 int main()
 {
+	int size;
+	cin >> size;
+	int freq[10000]={0};
+	for (int i = 0; i < size; ++i){
+		int num;
+		cin >> num;
+		freq[num]++;
+	}
+	int median = (size+1)/2, start = 0; // this value is the median
+	for (int i = 0; i < 10000; ++i)
+	{
+		start += freq[i];
+		if(start >= median)
+		{
+			cout << i;
+			break;
+		}
+	}
 	return 0;
 }
