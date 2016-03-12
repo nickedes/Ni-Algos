e1e14813b55081c198451d42cb00217786114677
nikhil <nkmittal4994@gmail.com>
2016-03-12 17:03:17 +0530

    updated frequency counts!

--- Practice/Hr/Sorting/Counting_Sort1.cpp
+++ Practice/Hr/Sorting/Counting_Sort1.cpp
@@ -7,5 +7,14 @@ using namespace std;
 
 int main()
 {
-	
+	int size, arr[100]={0};
+	cin >> size;
+	for (int i = 0; i < size; ++i)
+	{
+		int num;
+		cin >> num;
+		arr[num]++;
+	}
+	for (int i = 0; i < 100; ++i)
+		cout << arr[i] << " ";
 }
