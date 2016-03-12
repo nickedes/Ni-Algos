3a777e04bd44b27566982ef9fa2fe6e93f3bea12
nikhil <nkmittal4994@gmail.com>
2016-03-12 19:42:51 +0530

    Implementation probs

---
+++ Practice/Hr/Implementation/Angry_Professor.cpp
@@ -0,0 +1,31 @@
+/*
+Problem : https://www.hackerrank.com/challenges/find-median
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int tests;
+	cin >> tests;
+	while(tests--)
+	{
+		int N, K;
+		cin >> N >> K;
+		int arr[N], count = 0;
+		for (int i = 0; i < N; ++i)
+		{
+			int x;
+			cin >> x;
+			if(x <= 0)
+				count++;
+		}
+		if(count >= K)
+			cout << "NO";
+		else
+			cout << "YES";
+		cout << endl;
+	}
+	return 0;
+}
