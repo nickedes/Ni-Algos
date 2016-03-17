788f54c63cce19f2d5b0cb7ece0e1646cae925c1
nikhil <nkmittal4994@gmail.com>
2016-03-18 02:07:23 +0530

    Height of binary tree

--- Practice/Hr/Data Structures/Trees/heightBinaryTree.cpp
+++ Practice/Hr/Data Structures/Trees/heightBinaryTree.cpp
@@ -1,5 +1,7 @@
+/*
+Problem : https://www.hackerrank.com/challenges/tree-inorder-traversal
 
-/*The tree node has data, left child and right child 
+The tree node has data, left child and right child 
 struct node
 {
     int data;
@@ -10,6 +12,15 @@ struct node
 */
 int height(node * root)
 {
-  
+	int h_left = 0, h_right = 0;
+	if(root->left)
+		h_left = 1 + height(root->left);
+	if(root->right)
+		h_right = 1 + height(root->right);
+	if(!root->left && !root->right)
+		return 1;
+	if(h_left >= h_right)
+		return h_left;
+	else
+		return h_right;
 }
-  
