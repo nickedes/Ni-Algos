37769bf03fff77438147d2bef5179cb619e17c80
nikhil <nkmittal4994@gmail.com>
2016-03-18 14:29:29 +0530

    Swapping of nodes at the depth

--- Practice/Hr/Data Structures/Trees/SwapNodes.cpp
+++ Practice/Hr/Data Structures/Trees/SwapNodes.cpp
@@ -9,6 +9,23 @@ struct node
    node * right;
 };
 
+void Inorder(node *root, int cur_depth, int k)
+{
+	if(root == NULL)
+		return;
+	if(cur_depth % k == 0)
+	{
+		node *temp = root->left;
+		root->left = root->right;
+		root->right = temp;
+	}
+	if(root->left)
+		Inorder(root->left, cur_depth+1, k);
+	cout << root->data << " ";
+	if(root->right)
+		Inorder(root->right, cur_depth+1, k);
+}
+
 node * newNode(int data)
 {
 	node *ptr = new node;
@@ -43,6 +60,14 @@ int main()
 			q.push(r_child);
 		q.pop();
 	}
-	
+	int T;
+	cin >> T;
+	while(T--)
+	{
+		int k;
+		cin >> k; // depth for swap!
+		Inorder(root, 1, k);
+		cout << endl;
+	}
 	return 0;
 }
