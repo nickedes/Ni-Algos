4204e3e107abe959be7c7873c12cc10043abce93
nikhil <nkmittal4994@gmail.com>
2017-06-26 15:40:58 +0530

    Learning graph problem in python

--- graph/learningGraph.cpp
+++ graph/learningGraph.cpp
@@ -1,2 +1,9 @@
 // https://www.hackerearth.com/practice/algorithms/graphs/graph-representation/practice-problems/algorithm/monk-learning-graph-3/description/
-// 
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() 
+{
+	
+}
---
+++ graph/learningGraph.py
@@ -0,0 +1,19 @@
+# https://www.hackerearth.com/practice/algorithms/graphs/graph-representation/practice-problems/algorithm/monk-learning-graph-3/description/
+n, m, k = [int(x) for x in input().split()]
+
+val = [int(x) for x in input().split()]
+
+graph = dict()
+for i in range(1, n+1):
+    graph[i] = list()
+
+for i in range(m):
+    x, y = [int(z) for z in input().split()]
+    graph[x].append(y)
+    graph[y].append(x)
+
+for i in range(1, n+1):
+    if len(graph[i]) >= k:
+        print(sorted(graph[i], key=lambda s: val[s-1], reverse=True)[k-1])
+    else:
+        print(-1)
