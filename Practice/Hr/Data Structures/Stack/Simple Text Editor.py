137ff9671cab24ac55e857a00965be454e198cd7
nikhil <nkmittal4994@gmail.com>
2017-07-07 10:33:16 +0530

    editor

--- Practice/Hr/Data Structures/Stack/Simple Text Editor.py
+++ Practice/Hr/Data Structures/Stack/Simple Text Editor.py
@@ -1,15 +1,19 @@
 s = ''
 q = int(input())
+history = []
 for i in range(q):
     string = input()
     if string == '4':
-        print(string)
+        s = history.pop()
     else:
         string = string.split()
         if string[0] == '1':
+            history.append(s)
             s += string[1]
         elif string[0] == '2':
+            history.append(s)
             k = int(string[1])
-
+            s = s[:-k]
         elif string[0] == '3':
             k = int(string[1])
+            print(s[k-1])
