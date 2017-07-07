4dce1cd8d430ac31d7f710171272df5338d1649e
nikhil <nkmittal4994@gmail.com>
2017-07-07 09:56:39 +0530

    dummy

---
+++ Practice/Hr/Data Structures/Stack/Simple Text Editor.py
@@ -0,0 +1,15 @@
+s = ''
+q = int(input())
+for i in range(q):
+    string = input()
+    if string == '4':
+        print(string)
+    else:
+        string = string.split()
+        if string[0] == '1':
+            s += string[1]
+        elif string[0] == '2':
+            k = int(string[1])
+
+        elif string[0] == '3':
+            k = int(string[1])
