3dd2ce9db28c102a7a841193784e008a6b7f9d5d
nikhil <nkmittal4994@gmail.com>
2017-07-11 18:35:40 +0530

    bfs

--- Practice/Hr/Data Structures/Queues/Castle on the Grid.py
+++ Practice/Hr/Data Structures/Queues/Castle on the Grid.py
@@ -1,15 +1,33 @@
 from queue import Queue
 
 
-def bfs(s, n, r):
+def bfs(s, n, r, graph):
     # s - source
     # r - reaching point
     S = Queue()
     S.put(s)
+    visited = set()
+    a, b = s[0:2]
+    S.put((a, b + 1, 1, 1))
+    S.put((a + 1, b, 0, 1))
+    S.put((a, b - 1, 1, 1))
+    S.put((a - 1, b, 0, 1))
+    visited.add((a, b))
     while not S.empty():
         node = S.get()
-        print(node)
-        pass
+        a, b, direct, cost = node
+        if (a, b) not in visited and a >= 0 and a < n and b >= 0 and b < n:
+            if not graph[a][b]:
+                continue
+            if (a, b) == r:
+                print(cost)
+                break
+            # print(node)
+            S.put((a, b + 1, 1, cost + 1 if direct == 0 else cost))
+            S.put((a + 1, b, 0, cost + 1 if direct == 1 else cost))
+            S.put((a, b - 1, 1, cost + 1 if direct == 0 else cost))
+            S.put((a - 1, b, 0, cost + 1 if direct == 1 else cost))
+            visited.add((a, b))
 
 
 n = int(input())
@@ -26,4 +44,4 @@ for i in range(n):
 
 a, b, c, d = list(map(int, input().split()))
 
-bfs((a, b, 0), n, (c, d))
+bfs((a, b, 0, 0), n, (c, d), graph)
